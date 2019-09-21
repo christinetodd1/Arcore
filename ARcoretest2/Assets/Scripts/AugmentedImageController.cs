176906fcfb68557e01faa3713ff4a990e97703b6
christinetodd1 <43075375+christinetodd1@users.noreply.github.com>
2019-09-21 17:05:53 -0600

    change

---
+++ ARcoretest2/Assets/Scripts/AugmentedImageController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class AugmentedImageController : MonoBehaviour
+{
+    //serialized meaning, can input this from unity, we will drag it in, and this object will be available to the controller
+    [SerializeField] private AugmentedImageVisualizer _augmentedImageVisualizer;
+
+    private readonly Dictionary<int, AugmentedImageVisualizer> _visualizers =
+        new Dictionary<int, AugmentedImageVisualizer>(); 
+    //keep an instantiated list of visualizers that we currently have so we dont have to keep creating new ones.
+    private readonly List<AugmentedImage> _images = new List<AugmentedImage>();//list of images that we are tracking
+
+private void Update()
+{
+
+    if (Session.Status != SessionStatus.Tracking)
+    {
+        return;
+    }
+
+    Session.GetTrackables(_images, TrackableQueryFilter.Updated);
+    VisualizeTrackables();
+    //when we find an imnage we will put something on top of that. track:get ahold of them and put the visualizers on top of them.
+}
+private void VisualizeTrackables()
+{
+    foreach (var image in _images)
+    {
+        var visualizer = GetVisualizer(image);
+        if (image.TrackingState == TrackingState.Tracking && visualizer == null)
+        {
+            AddVisualizer(image);//if tracking add augment
+        }
+        else if (image.TrackingState == TrackingState.Stopped && visualizer != null)
+
+        {
+            RemoveVisualizer(image, visualizer);// if it has lost tracking remove te augment
+           
+        }
+    }
+}
+
+    private void RemoveVisualizer(AugmentedImage image, AugmentedImageVisualizer visualizer)
+
+{
+    _visualizers.Remove(image.DatabaseIndex);
+    Destroy(visualizer.gameObject);
+}
+
+private void AddVisualizer(AugmentedImage image)
+{
+    var anchor = image.CreateAnchor(image.CenterPose);
+    var visualizer = Instantiate(_augmentedImageVisualizer, anchor.transform);
+    _visualizers.Add(image.DatabaseIndex, visualizer);
+
+}
+
+private AugmentedImageVisualizer GetVisualizer(AugmentedImage image)
+{
+    AugmentedImageVisualizer visualizer;
+    _visualizers.TryGetValue(image.DatabaseIndex, out visualizer);
+
+    return visualizer;
+
+}
+    }
+
+
+
+
+            
---
+++ ARcoretest2/Assets/Scripts/AugmentedImageVisualizer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using GoogleARCore;
+
+
+
+public class AugmentedImageVisualizer : MonoBehaviour {
+    [SerializeField] private VideoClip[] _videoClips;//array of video clips
+    public AugmentedImage Image;
+    private VideoPlayer _videoPlayer;
+
+    void Start()
+    {
+        _videoPlayer = GetComponent<VideoPlayer>();
+        _videoPlayer.loopPointReached += OnStop;//Stops video
+
+    }
+
+    private void OnStop(VideoPlayer source)
+    {
+        gameObject.SetActive(false);//makes video disappear
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Image == null || Image.TrackingState != TrackingState.Tracking)
+        {
+            return;//making sure the image and tracking is there to even run
+        }
+
+        if (!_videoPlayer.isPlaying)
+        {
+            _videoPlayer.clip = _videoClips[Image.DatabaseIndex];
+            _videoPlayer.Play();
+            //if our video isnt playing we will play the correct one in our array
+        }
+        transform.localScale = new Vector3(Image.ExtentX, Image.ExtentZ, 1); //needs to change the dimesions of the image every frame
+    }
+}
